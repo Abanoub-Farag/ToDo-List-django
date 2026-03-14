a7f0c37fb452611a5d9819fbc0aea09f76e81317
Abanoub-Farag <abanoubfarag02@gmail.com>
2026-03-14 19:00:03 +0200

    feat: Added task delete functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_page_view, create_task_view
+from .views import home_page_view, create_task_view, delete_task
 
 
 urlpatterns = [
+    path("delete_task/<id>/", delete_task, name="delete_task"),
     path("create_task/", create_task_view, name="create_task"),
     path("", home_page_view, name="home")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
@@ -20,3 +20,9 @@ def create_task_view(request):
         return redirect("home")
     
     return render(request, "create_task.html")
+
+
+def delete_task(request, id):
+    task = get_object_or_404(tasks, id=id)
+    task.delete()
+    return redirect("/")
