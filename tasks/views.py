708636fad79ea16ffd750bf79df7b4facb643046
Abanoub-Farag <abanoubfarag02@gmail.com>
2026-03-18 11:26:59 +0200

    refactor: convert some function-based views to class-based views

--- tasks/views.py
+++ tasks/views.py
@@ -4,13 +4,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from django.views.generic.edit import DeleteView
+from django.views import View
 
-@login_required
-def home_page_view(request):
-    objects = Task.objects.filter(user=request.user)
-    context = {"tasks" : objects}
-    return render(request, "home.html", context)
+# @login_required
+# def home_page_view(request):
+#     objects = Task.objects.filter(user=request.user)
+#     context = {"tasks" : objects}
+#     return render(request, "home.html", context)
 
+class HomePageView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "home.html", {"tasks" : Task.objects.filter(user=request.user)})
 
 @login_required
 def create_task_view(request):
@@ -26,11 +35,20 @@ def create_task_view(request):
     
     return render(request, "create_task.html")
 
-@login_required
-def delete_task(request, id):
-    task = get_object_or_404(Task, id=id, user=request.user)
-    task.delete()
-    return redirect("/")
+# @login_required
+# def delete_task(request, id):
+#     task = get_object_or_404(Task, id=id, user=request.user)
+#     task.delete()
+#     return redirect("/")
+
+
+class DeleteTaskView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = "/"
+    template_name = "task_confirm_deletion.html"
+    pk_url_kwarg = 'id'
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
 
 @login_required
 def update_task_view(request, id):
